fb4eae7e379c2d5aa393572260886b4a4963b68d
Lucy Toman <lucy@toman.me.uk>
2021-03-10 12:38:22 +0100

    initial commit of textmess

---
+++ script.js
@@ -0,0 +1,52 @@
+const input = document.getElementById("inbox");
+const output = document.getElementById("outbox");
+let shufInnerStr = "";
+let shufWord = [];
+let first = "";
+let x;
+let final = "";
+let resultArr = [];
+
+function messText() {
+    let text = input.value;
+
+    let string = text.split(" ");
+
+    string.forEach((item) => {
+        let word = item.split("");
+        let inner = [];
+        let ins;
+
+        first = word[0];
+        x = word.length - 1;
+        final = word[x];
+
+        for (i = 1; i < word.length - 1; i++) {
+            ins = word[i];
+            inner.push(ins);
+        }
+
+        shuffleArray(inner);
+
+        result = shufWord.join("");
+    });
+
+    output.innerHTML = result;
+}
+
+function shuffleArray(array) {
+    if (array.length < 2) {
+        console.log("array", array);
+    } else {
+        for (let i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    shufInnerStr = array.join("");
+    shufWord.push(first, shufInnerStr, final, " ");
+
+    return shufWord;
+}
