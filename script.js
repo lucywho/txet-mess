772ba8a267c6b5901cd58dce6e1565f1f47e6957
Lucy Toman <lucy@toman.me.uk>
2021-03-10 13:08:03 +0100

    fixes glitch with single letter words

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ function messText() {
         let ins;
 
         first = word[0];
-        x = word.length - 1;
+        if (word.length <= 1) {
+            x = word.length;
+        } else {
+            x = word.length - 1;
+        }
         final = word[x];
 
         for (i = 1; i < word.length - 1; i++) {
@@ -37,17 +41,16 @@ function messText() {
 function shuffleArray(array) {
     if (array.length > 1) {
         for (let i = array.length; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = array[i];
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = array[i];
             array[i] = array[j];
             array[j] = temp;
         }
     }
+
     shufInnerStr = array.join("");
+
     shufWord.push(first, shufInnerStr, final, " ");
 
     return shufWord;
 }
-
-// array = array;
-//} else {
