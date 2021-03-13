aab215b054e49c17e9cc1488c24d5b22e46ba2ab
Lucy Toman <lucy@toman.me.uk>
2021-03-13 16:47:46 +0100

    removes punctuation from string, prevents about overlapping close button, adds conditional rendering of buttons

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// button code
+
+const messButton = document.getElementById("mess_button");
+const againButton = document.getElementById("again_button");
+
+messButton.innerHTML =
+    '<button type="button" onclick="messText()">Mess it up</button>';
+
+againButton.innerHTML =
+    '<button type="button" onclick="againGone()">Mess again</button>';
+
+function againGone() {
+    againButton.classList.remove("vis");
+    againButton.classList.add("invis");
+
+    location.reload();
+}
+
 // word scramble code
 
 const input = document.getElementById("inbox");
@@ -12,13 +30,17 @@ let resultArr = [];
 
 function messText() {
     let txt = input.value;
+    txt = txt.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
 
     if (!txt) {
         output.innerHTML = `please enter some text in the pink box`;
     } else {
-        let string = txt.split(" ");
+        messButton.classList.add("invis");
+        messButton.classList.remove("vis");
 
-        string.forEach((item) => {
+        let stringArr = txt.split(" ");
+
+        stringArr.forEach((item) => {
             let word = item.split("");
             let inner = [];
             let ins;
@@ -40,11 +62,11 @@ function messText() {
 
             result = shufWord.join("");
         });
-        output.innerText = "";
-        console.log("out", output.innerText);
 
         output.value = result;
-        console.log("out after result", output.value);
+
+        againButton.classList.add("vis");
+        againButton.classList.remove("hidden");
     }
 }
 
@@ -87,7 +109,7 @@ function toggleAbout() {
 
 aboutText.innerHTML = `It is a meme universally acknowledged that a word in possession of all its letters can still be easily read if the middle letters are scrambled, because the human brain reads the whole word not the individual letters.<br> <br>
 It is, perhaps, less universally acknowledged that an unemployed web dev in possession of a bored husband during a pandemic is going to be given some weird shit to build. So here we are...<br><br>
-<b>What this app does</b> <br>The site will scramble your text, retaining the first and last letter of each word in the correct position. Obviously, this doesn't work on words of 3 letters or less, and there's a chance that other short words will appear to be unscrambled: for example, a 4-letter word only has two inner letters to scramble, so there's a 50% chance it will look exactly the same after randomising.<br><br>If you enter anything much more complex than "the quick brown fox..." you will realise quite quickly that the meme is not entirely accurate. Yes, fluent readers do tend to read the word as whole rather than reading each individual letter, but it takes about 11% longer and there are various other issues that affect our reading comprehension of jumbled words. For example, it is much easier to read words where the letters are transposed rather than completely scrambled, or where the overall sound of the word is not much altered by the scrambling (e.g. daeilng is easier to read as "dealing" than dalneig). Longer words rapidly become increasingly tricky acronymn puzzles if scrambled randomly.
+<b>What this app does</b> <br>The site will scramble your text, retaining the first and last letter of each word in the correct position. Obviously, this doesn't work on words of 3 letters or less, and there's a chance that other short words will appear to be unscrambled: for example, a 4&#8209letter word only has two inner letters to scramble, so there's a 50% chance it will look exactly the same after randomising.<br><br>If you enter anything much more complex than "the quick brown fox..." you will realise quite quickly that the meme is not entirely accurate. Yes, fluent readers do tend to read the word as whole rather than reading each individual letter, but it takes about 11% longer and there are various other issues that affect our reading comprehension of jumbled words. For example, it is much easier to read words where the letters are transposed rather than completely scrambled, or where the overall sound of the word is not much altered by the scrambling (e.g. daeilng is easier to read as "dealing" than dalneig). Longer words rapidly become increasingly tricky acronymn puzzles if scrambled randomly.
 <br><br><b>References</b><br>Rawlinson, G.E.(1976) <i>The Significance of Letter Position in Word Recognition</i> [Unpublished doctoral thesis]. University of Nottingham <br> <a href="https://www.mrc-cbu.cam.ac.uk/people/matt.davis/Cmabrigde/rawlinson/" target="_">Summary available online <a> at the University of Cambridge MRC Cognition and Brain Sciences Unit website<br><br>
 Rayner, K., White, S.J., Johnson, R.L and Liversedge, S.P (2006) <i>Raeding Wrods With Jubmled Lettres There Is a Cost</i>
 Psychological Science, 17(3), 192-193<br><br><b>Website</b> <br> &copy <a href="https://lucysco.de/" target="_">Lucy Toman 2021</a> <br> From an original idea by <a href="https://trashpanda.cc/" target="_">Tim Duckett </a><br><br> `;
